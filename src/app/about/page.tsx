41bb0215c90f72854a4c7726e27d353e1ae362d0
hari-gitacc <harisrini777@gmail.com>
2025-06-06 22:07:49 +0530

    fix:account page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Header from '@/components/Header';
-import { motion } from 'framer-motion';
+import Header from '@/components/Header'
+import { motion } from 'framer-motion'
 
 export default function AboutPage() {
   return (
